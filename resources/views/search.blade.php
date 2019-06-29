31ed81f49e10c61ea53d43e30d18169634fdfcd1
abdalgaderserag <abdalgadersir@gmail.com>
2019-06-29 15:48:16 -0700

    create the sass file for the search view have some trouble with main-body class start working on it

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.app')
 
+@section('style')
+    <link rel="stylesheet" href="{{ asset('css/search.css') }}">
+@endsection
+
 @section('content')
-    <div class="main">
-        asd,mas.
+    <div class="main-content">
+
     </div>
 @endsection
