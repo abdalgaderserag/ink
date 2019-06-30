2185e30dad91db7c7fe5357bdd45fc1fced100e7
abdalgaderserag <abdalgadersir@gmail.com>
2019-06-29 19:50:14 -0700

    add the main file that used to add the languge for the site

---
+++ resources/lang/en/account.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'ink' => 'Ink .',
+    'sign in' => 'Sign In',
+    'sign up' => 'Sign Up',
+    'new' => 'New to Ink?',
+    'new join' => 'Join our huge communite today by sign up in the form blow.',
+    'log' => 'Login',
+    'reg' => 'Register',
+
+];
---
+++ resources/lang/en/main.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    'sug' => 'Sugested',
+    'i_s' => 'Inks and shared',
+    'i' => 'Inks',
+    'images' => 'Images',
+    'star' => 'Stared',
+    'followers' => 'Followers',
+    'followed' => 'Followed',
+    'ink' => 'Ink.',
+    'search' => 's e a r c h',
+    'filter' => 'Filters',
+    'create' => 'Create',
+    'it' => 'it!',
+    'reply' => 'Reply',
+    'share' => 'Share',
+    'delete' => 'Delete',
+    'edit' => 'Edit',
+];
