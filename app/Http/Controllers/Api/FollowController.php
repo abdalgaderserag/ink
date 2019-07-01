fa9636cd3c84483affb3e10048b33a2ef9bf1dad
abdalgaderserag <abdalgadersir@gmail.com>
2019-06-30 17:12:07 -0700

    creating the event and the listenr for the like and comment

--- app/Http/Controllers/Api/FollowController.php
+++ app/Http/Controllers/Api/FollowController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use App\Follow;
 use App\Http\Controllers\Controller;
+use App\Notifications\NewFollower;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -18,14 +20,18 @@ class FollowController extends Controller
     public function __invoke(Request $request)
     {
         $follow = Follow::where('follower_id', $request->id)->where('followed_slug', Auth::user()->slug)->first();
+        $user = User::find($request->id);
         if (empty($follow)) {
             $follow = new Follow();
             $follow->follower_id = $request->id;
             $follow->followed_slug = Auth::user()->slug;
             $follow->save();
+            $user->notify(new NewFollower(Auth::user()->slug));
             return response(1, 200);
         } else {
             $follow->delete();
+            $note = $user->notifications->where('type','App\Notifications\NewFollower')->where('notifiable_id',$user->id)->first();
+            $note->delete();
             return response(0, 200);
         }
     }
