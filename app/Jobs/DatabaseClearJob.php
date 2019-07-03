5c638260162b55da2a3eff9c7d490f2f9cc2c549
abdalgaderserag <abdalgadersir@gmail.com>
2019-07-03 09:42:17 -0700

    start working in the database clear job to handle unused data

---
+++ app/Jobs/DatabaseClearJob.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Jobs;
+
+use App\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class DatabaseClearJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $user_id;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param $id
+     * @return void
+     */
+    public function __construct($id)
+    {
+        $this->user_id = $id;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $user = User::find($this->user_id);
+        foreach ($user->notifications as $notification) {
+            $notification->delete();
+        }
+
+        foreach ($user->)
+    }
+}
